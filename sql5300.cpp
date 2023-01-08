92852af643429865c1f15f8e5988d06bd4f29f5c
Justin Thoreson <thoresonjd@gmail.com>
2023-01-08 14:57:27 -0800

    Basic SQL shell loop

--- sql5300.cpp
+++ sql5300.cpp
@@ -19,7 +19,37 @@ int main(int argc, char** argv) {
   env.set_message_stream(&std::cout);
   env.set_error_stream(&std::cerr);
   env.open(ENV_DIR.c_str(), ENV_FLAGS, 0);
+
+  // Establish database
+  Db db(&env, 0);
+  db.set_message_stream(env.get_message_stream());
+  db.set_error_stream(env.get_error_stream());
+  db.set_re_len(BLOCK_SZ);
+  db.open(NULL, SQL_5300.c_str(), NULL, DB_RECNO, DB_FLAGS, 0);
+
+  // Write block to db
+  char block[BLOCK_SZ];
+  Dbt data(block, sizeof(block));
+  int block_number;
+  Dbt key(&block_number, sizeof(block_number));
+  block_number = 1;
+  strcpy(block, "Hello, DB!");
+  db.put(NULL, &key, &data, 0);
+
+  // Read block from db
+  Dbt rData;
+  db.get(NULL, &key, &rData, 0);
+  std::cout << "Read (block #" << block_number << "): '" << (char *)rData.get_data() << "'";
+	std::cout << " (expect 'hello!')" << std::endl;
+
+  // SQL shell
   std::cout << "(sql5300: running with database environment at " << ENV_DIR << std::endl;
-  std::cout << "SQL> ";
+  
+  std::string query = "";
+  while (query != "quit") {
+    std::cout << "SQL> ";
+    std::cin >> query;
+  }
+
   return EXIT_SUCCESS;
 }
