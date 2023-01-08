caf791d9275b2e84b1a54ec2dacaad015aaa669e
Justin Thoreson <thoresonjd@gmail.com>
2023-01-08 14:49:50 -0800

    Basic print statement setup

--- sql5300.cpp
+++ sql5300.cpp
@@ -9,43 +9,17 @@ const std::string SQL_5300 = "sql5300.db";
 const unsigned int BLOCK_SZ = 4096;
 
 int main(int argc, char** argv) {
-  // Retrieve command line arguments
   if (argc != 2) {
-    std::cout << "USAGE: ./ass [database directory]\n";
+    std::cout << "USAGE: " << argv[0] << " [db_environment]\n";
     return EXIT_FAILURE;
   }
-
-  // Print environment directory
-  const std::string ENV_DIR = argv[1];
-  std::cout << ENV_DIR << std::endl;
-
   // Establish database environment
+  const std::string ENV_DIR = argv[1];
   DbEnv env(0U);
   env.set_message_stream(&std::cout);
   env.set_error_stream(&std::cerr);
   env.open(ENV_DIR.c_str(), ENV_FLAGS, 0);
-
-  // Establish database
-  Db db(&env, 0);
-  db.set_message_stream(env.get_message_stream());
-  db.set_error_stream(env.get_error_stream());
-  db.set_re_len(BLOCK_SZ);
-  db.open(NULL, SQL_5300.c_str(), NULL, DB_RECNO, DB_FLAGS, 0);
-
-  // Write block to db
-  char block[BLOCK_SZ];
-  Dbt data(block, sizeof(block));
-  int block_number;
-  Dbt key(&block_number, sizeof(block_number));
-  block_number = 1;
-  strcpy(block, "Hello, DB!");
-  db.put(NULL, &key, &data, 0);
-
-  // Read block from db
-  Dbt rData;
-  db.get(NULL, &key, &rData, 0);
-  std::cout << "Read (block #" << block_number << "): '" << (char *)rData.get_data() << "'";
-	std::cout << " (expect 'hello!')" << std::endl;
-
+  std::cout << "(sql5300: running with database environment at " << ENV_DIR << std::endl;
+  std::cout << "SQL> ";
   return EXIT_SUCCESS;
 }
