9370d30fcdc7ef6e1cca870b69ba340b452c7ffc
Justin Thoreson <thoresonjd@gmail.com>
2023-01-08 13:34:34 -0800

    sqlshell.cpp takes environment directory as command line argument

--- sqlshell.cpp
+++ sqlshell.cpp
@@ -2,7 +2,12 @@
 #include <iostream>
 #include "db_cxx.h"
 
-int main(void) {
-  std::cout << "Hello, world!\n";
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cout << "USAGE: ./ass [database directory]\n";
+    return EXIT_FAILURE;
+  }
+  const std::string ENV_DIR = argv[1];
+  std::cout << ENV_DIR << std::endl;
   return EXIT_SUCCESS;
 }
